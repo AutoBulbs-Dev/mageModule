1ac1d00006313e52bcdfdf776cff89f3191c7484
user <user@users-MacBook-Pro.local>
2016-05-10 09:42:16 +0100

    Adding New Products Module

---
+++ Newproducts/Block/Newproducts.php
@@ -0,0 +1,8 @@
+<?php
+namespace ABD\Newproducts\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Newproducts extends Template
+{
+}
---
+++ Newproducts/registration.php
@@ -0,0 +1,8 @@
+
+<?php
+	\Magento\Framework\Component\ComponentRegistrar::register(
+		\Magento\Framework\Component\ComponentRegistrar::
+		MODULE,
+		'ABD_Newproducts',
+		__DIR__ 
+	);
